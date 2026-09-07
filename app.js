28e105fffa5b37d26d8dbd5c16fe20894cd09403
Elyone Olateru <ogoola16@gmail.com>
2026-09-07 15:36:06 +0100

    Fix Railway server port

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ require("./mqtt");
 // =========================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
