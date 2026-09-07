78b47169b8085c1a6c82741ab65d2e7d3d9c594f
Elyone Olateru <ogoola16@gmail.com>
2026-09-07 15:26:42 +0100

    Fix Railway dependencies

--- app.js
+++ app.js
@@ -101,19 +101,25 @@ app.get("/", (req, res) => {
 
 });
 
+
+//HEALTH CHECK
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
 // =========================
 // MQTT
 // =========================
 
 require("./mqtt");
 
-
 // =========================
 // START SERVER
 // =========================
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 
